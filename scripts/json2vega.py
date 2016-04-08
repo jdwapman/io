d4d1cff2b225d07f9c87cb52d6584c7ae2ee47bc
ffarhour <f.farhour@gmail.com>
2016-04-08 08:41:59 -0700

    create output dir if not exist. added write_json method to write vega spec json file outputs

--- scripts/json2vega.py
+++ scripts/json2vega.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 #Author: Farmehr Farhour f.farhour@gmail.com
+#Some code refactored and re-used from testvl.py script written by JDO
 
-import pandas, numpy, json, os
-import sys, argparse
+import pandas, numpy
+import json, os, sys, argparse  #built-in
+from subprocess import Popen, PIPE, STDOUT # built-in
 
 #class: bcolors
 #Class for coloring terminal outputs using ANSI color codes
@@ -17,8 +19,17 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+#function: write_json
+#write json to a specified file.
+def write_json(json_in, json_name):
+    # pipe it through vl2vg to turn it into vega
+    file = open('%s_g_%s.json' %(args.o,json_name), 'w')
+    p = Popen(["vl2vg"], stdout=file, stdin=PIPE)
+    vg = p.communicate(input=json.dumps(json_in))[0]
+    file.close()
 
 #function: inputArgs
+#Processes input arguments
 def inputArgs(argv):
     #define global variablesbeing used as input args
     global g_iDir
@@ -26,7 +37,8 @@ def inputArgs(argv):
     #parse args
     parser = argparse.ArgumentParser(description=bcolors.HEADER + 'IO Options' + bcolors.ENDC)
     parser.add_argument('-d', metavar='<directory>', type=str, help='directory containing input JSON files', default='/')
-    parser.add_argument('-o', metavar='<directory>', type=str, help='directory for output files', default='/output/')
+    parser.add_argument('-o', metavar='<directory>', type=str, help='directory for output files', default='output/')
+    global args
     args = parser.parse_args()
 
 #---<<MAIN function starts here>>---
@@ -37,13 +49,30 @@ def main(argv):
 
     #process input arguments passed
     inputArgs(argv)
+    #create output directory
+    if not os.path.exists(args.o):
+        os.makedirs(args.o)
 
-    #function: User_Input
-    #In:    disp_name: display name of the variable required to ask the user.
-    #In:    default_val: default value of the variable
-    #Out:   variable value as given by user
-    def User_Input(disp_name, default_val):
-        print "hello"
+
+#TESTS
+    ## Sample bar graph json for testing
+    bar = {
+        "mark": "point",
+        "encoding": {
+            "y": {"scale": {"type": "log"},
+                  "type": "quantitative",
+                  "field": "m_teps",
+                  "axis": {
+                      "title": "MTEPS"
+                  }
+            },
+            "x": {"type": "ordinal",
+                  "field": "dataset"
+            }
+        }
+    }
+    write_json(bar, "json_name")
+
 
 if __name__ == "__main__":
    main(sys.argv[1:])
