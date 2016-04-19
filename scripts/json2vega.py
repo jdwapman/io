d00af7630f5494ae3e5b69a6bfe7f3483f5e05ae
ffarhour <f.farhour@gmail.com>
2016-04-19 00:16:12 -0700

    COmpleted all docstrings for the code so far

--- scripts/json2vega.py
+++ scripts/json2vega.py
@@ -8,7 +8,7 @@ This file contains a base class, and a child class for each type of graph to be 
 """
 
 import pandas, numpy
-import json, os, sys, argparse          #built-in
+import json, os, argparse          #built-in
 from subprocess import Popen, PIPE, STDOUT          #built-in
 
 class bcolors:
@@ -77,32 +77,73 @@ class VegaGraphBase(object):
         """Returns the json config file as a python object"""
         return json.load(open(self.config_path + "bar_config.json"))
 
-    #function: parse_jsons
+
     def parse_jsons(self):
+        """Parses the input json files using Pandas.
+
+        Returns: a pandas dataframe containting the data processed from input jsons.
+        """
         #store all data in a pandas DataFrame
         pandas_df = pandas.DataFrame(self.__input_jsons)
         return pandas_df
-    #function: write_json
-    #output json to the output_path and with a specific name
+
+
     def write_json(self,json_in,suffix=""):
+        """Output json to the output_path and with a specific name.
+
+        The filename is in the format: '_<engine_name>_<algorithm_name>_suffix.json'.
+        This method uses vega-lite to produce the final vega-spec output.
+
+        Arguments:
+            json_in: the input json file to be outputted to file.
+            suffix: the suffix to name the output files with. default is ""
+        """
+
         # pipe it through vl2vg to turn it into vega
         file = open('%s_%s_%s_%s.json' %(self.output_path,self.engine_name,self.algorithm_name,suffix), 'w')
         p = Popen(["vl2vg"], stdout=file, stdin=PIPE)
         vg = p.communicate(input=json.dumps(json_in))[0]
         file.close()
 
-#Class to produce vega bar graphs
-class VegaGraphBar(VegaGraphBase):
-    'Class to produce bar graphs from specified data'
 
-    #Constructor
+class VegaGraphBar(VegaGraphBase):
+    """Class for converting json outputs of different algorithms to vega-specific bar graph json files.
+
+    This class is a child class of VegaGraphBase and inherits all the methods and variables.
+
+    Attributes:
+        output_path: the output directory to write the vega-spec json files to.
+        input_path: the input path containing the input json files to be processed.
+        config_path: the directory containing the json config files relevant to each plot type.
+            Each config file will need to have a specific name of the format: '<plot type>_config.json'
+        engine_name: the name of the engine used to run the algorithm (e.g. Gunrock). This is
+            used to name the output files.
+        algorithm_name: the name of the algorithm (e.g. BFS). This is used to name the
+            output files.
+        conditions_dict: a dictionary containing the conditions to limit the input files to a
+            specific category. For instance choosing files that were outputs of a BFS algorith.
+            For instance the following dictionary limits the inputs to BFS algorithms that are undirected and
+            mark_predecessors is true:
+            {"algorithm" : "BFS","undirected" : True ,"mark_predecessors" : True}
+        axes_vars: a dictionary of the variables to be evaluated for the x and y axes. There are 2 keys: 'x' and 'y'.
+            For instance:
+            {'x':'dataset','y':'m_teps'} would specify to the program to plot m_teps (on y-axis) vs. dataset (on x-axis)
+
+    """
+
+
     def __init__(self,output_path,input_path,config_path,engine_name,algorithm_name,conditions_dict,axes_vars):
+        """Instantiate the input arguments. References the base class __init__ to instantiate recurring ones."""
         self.conditions_dict = conditions_dict
         self.axes_vars = axes_vars
         super(VegaGraphBar,self).__init__(output_path,input_path,config_path,engine_name,algorithm_name)
 
 
     def parse_jsons(self):
+        """Parses the input json files using Pandas.
+
+        Returns: the json file to be written to file.
+        """
         pandas_df = super(VegaGraphBar,self).parse_jsons()
         #restricted bar graph, based on conditions_dict provided
         df_restricted = pandas_df
