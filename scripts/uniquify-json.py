b317cacb4fc97e263f36aab558ed451962737924
John Owens <jowens@ece.ucdavis.edu>
2016-10-30 09:15:55 -0700

    uniquifying JSON from YC's .tar.gz files

---
+++ scripts/uniquify-json.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import tarfile
+import json
+import glob
+from datetime import datetime
+
+cmdlines = {}
+
+for file in glob.glob('./*.tar.gz'):
+    tar = tarfile.open(file, "r:gz")
+    for member in [x for x in tar.getmembers() if
+                   (x.name.endswith(".json") and x.size > 0)]:
+        print file, member.name
+        jsonobj = json.load(tar.extractfile(member))
+
+        # 1 GPU only
+        if (jsonobj['num_gpus'] != 1):
+            # print "Multiple GPUs, skipping"
+            continue
+        cmdline = jsonobj['command_line']
+        gpu = jsonobj['gpuinfo']['name']
+        dt = datetime.strptime(jsonobj['time'], "%a %b %d %H:%M:%S %Y\n")
+
+        if (cmdline not in cmdlines):
+            cmdlines[cmdline] = {}
+        if ((gpu not in cmdlines[cmdline]) or
+                (cmdlines[cmdline][gpu] < dt)):
+            print "Inserting ", dt, " into ", cmdline
+            cmdlines[cmdline][gpu] = dt
+    tar.close()
+
+# hash now full
+for file in glob.glob('./*.tar.gz'):
+    tar = tarfile.open(file, "r:gz")
+    for member in [x for x in tar.getmembers() if
+                   (x.name.endswith(".json") and x.size > 0)]:
+        jsonobj = json.load(tar.extractfile(member))
+
+        # 1 GPU only
+        if (jsonobj['num_gpus'] != 1):
+            # print "Multiple GPUs, skipping"
+            continue
+        cmdline = jsonobj['command_line']
+        gpu = jsonobj['gpuinfo']['name']
+        dt = datetime.strptime(jsonobj['time'], "%a %b %d %H:%M:%S %Y\n")
+
+        if cmdlines[cmdline][gpu] == dt:
+            tar.extract(member, path="new/")
+            print "Writing: ", file, member.name
+    tar.close()
