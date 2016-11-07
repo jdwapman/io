85f6ce569abcc3b0d0548fb950dbf3284e18c3c5
John Owens <jowens@ece.ucdavis.edu>
2016-11-07 15:20:15 -0800

    added RGG/RMAT plots. Some pretty sloppy code, sorry to say.

--- scripts/altair_traversal_method.py
+++ scripts/altair_traversal_method.py
@@ -14,10 +14,10 @@ from fileops import savefile
 
 root = '../gunrock-output/'
 
-for prim in ['BC', 'PageRank', 'CC', 'SSSP', 'BFS']:
+for prim in ['BFS', 'BC', 'PageRank', 'CC', 'SSSP']:
 
-    json_files = [os.path.join(subdir, f) for (subdir, dirs, files)
-                  in os.walk(root) for f in files]
+    all_json_files = [os.path.join(subdir, f) for (subdir, dirs, files)
+                      in os.walk(root) for f in files]
 
     def filterFiles(f):
         return (os.path.isfile(f) and
@@ -25,16 +25,23 @@ for prim in ['BC', 'PageRank', 'CC', 'SSSP', 'BFS']:
                 os.path.basename(f).startswith(prim) and
                 not os.path.basename(f).startswith("_"))
 
-    json_files = filter(filterFiles, json_files)
+    all_json_files = filter(filterFiles, all_json_files)
 
-    # Filter only modified in Oct 2016 or later
-    def filterDate(f):
+    # Filter only modified in Oct 2016
+    def filterOct16(f):
         mtime = datetime.datetime.fromtimestamp(os.path.getmtime(f)).date()
-        return mtime > datetime.date(2016, 9, 30)
+        return ((mtime > datetime.date(2016, 9, 30)) and
+                (mtime < datetime.date(2016, 11, 1)))
 
-    json_files = filter(filterDate, json_files)
+    # Filter only modified on 7 Nov 2016
+    def filter7Nov16(f):
+        mtime = datetime.datetime.fromtimestamp(os.path.getmtime(f)).date()
+        return mtime == datetime.date(2016, 11, 7)
 
-    data_unfiltered = [json.load(open(jf)) for jf in json_files]
+    json_files_oct16 = filter(filterOct16, all_json_files)
+    json_files_7nov16 = filter(filter7Nov16, all_json_files)
+
+    data_unfiltered = [json.load(open(jf)) for jf in json_files_oct16]
     df = pandas.DataFrame(data_unfiltered)
     # All prim-specific data is now stored in the pandas DataFrame "df".
 
@@ -73,23 +80,61 @@ for prim in ['BC', 'PageRank', 'CC', 'SSSP', 'BFS']:
     for fileformat in ['html', 'svg', 'png']:
         savefile(chart, name=plotname % prim, fileformat=fileformat)
 
-    df['avg_node_degree'] = df['edges_visited'] / df['nodes_visited']
-    chart = Chart(df).mark_point().encode(
-        x=X('avg_node_degree',
-            axis=Axis(
-                title='Average node degree',
-            ),
-            ),
-        y=Y('m_teps',
-            axis=Axis(
-                title='MTEPS',
-            ),
-            # scale=Scale(type='log'),
-            ),
-        color=Color('traversal_mode:N',
-                    legend=Legend(title='%s / Traversal Mode' % prim),
+    # Average node degree chart
+    files = [(json_files_oct16, 'Real-World')]
+    if (prim == 'BFS'):
+        files.append((json_files_7nov16, 'RGG+RMAT'))
+    for file in files:
+        data_unfiltered = [json.load(open(jf)) for jf in file[0]]
+        df = pandas.DataFrame(data_unfiltered)
+
+        # "graph_type": "grmat",
+        # "graph_type": "rgg",
+
+        df['avg_node_degree'] = df['edges_visited'] / df['nodes_visited']
+        chart = Chart(df).mark_point().encode(
+            x=X('avg_node_degree',
+                axis=Axis(
+                    title='Average node degree',
+                ),
+                ),
+            y=Y('m_teps',
+                axis=Axis(
+                    title='MTEPS',
+                ),
+                # scale=Scale(type='log'),
+                ),
+            color=Color('traversal_mode:N',
+                        legend=Legend(
+                            title='%s / %s / Traversal Mode' % (file[1], prim)),
+                        ),
+        )
+        # also kludgey: why do this twice?
+        if file[1] == 'RGG+RMAT':
+            # this is kludgey
+            # http://stackoverflow.com/questions/19913659/pandas-conditional-creation-of-a-series-dataframe-column
+            df['RGG_vs_RMAT'] = ['RMAT' if gt ==
+                                 'grmat' else 'RGG' for gt in df['graph_type']]
+            chart = Chart(df).mark_text().encode(
+                x=X('avg_node_degree',
+                    axis=Axis(
+                        title='Average node degree',
                     ),
-    )
-    plotname = '%s_avg_node_degree'
-    for fileformat in ['html', 'svg', 'png']:
-        savefile(chart, name=plotname % prim, fileformat=fileformat)
+                    ),
+                y=Y('m_teps',
+                    axis=Axis(
+                        title='MTEPS',
+                    ),
+                    # scale=Scale(type='log'),
+                    ),
+                color=Color('traversal_mode:N',
+                            legend=Legend(
+                                title='%s / %s / Traversal Mode' % (file[1], prim)),
+                            ),
+                text='RGG_vs_RMAT:N',
+            )
+        plotname = '%s_%s_avg_node_degree'
+        for fileformat in ['html', 'svg', 'png']:
+            savefile(chart,
+                     name=plotname % (file[1], prim),
+                     fileformat=fileformat)
