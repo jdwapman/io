f295fbdaf508bb0337fa117140c3ba40ce9fb8b1
John Owens <jowens@ece.ucdavis.edu>
2016-11-24 13:49:54 -0800

    factored out filters into separate file

--- scripts/altair_topc.py
+++ scripts/altair_topc.py
@@ -4,7 +4,6 @@ from altair import *
 import pandas  # http://pandas.pydata.org
 from pandas.io.json import json_normalize
 import numpy
-import re      # built-in
 import json    # built-in
 import os      # built-in
 import copy    # built-in
@@ -12,69 +11,10 @@ import datetime
 from subprocess import Popen, PIPE, STDOUT, check_output, CalledProcessError
 
 from fileops import savefile
+from filters import *
 
-# possible filtering functions
-
-
-def fileEndsWithJSON(f):
-    return (os.path.isfile(f) and
-            (os.path.splitext(f)[1] == ".json") and
-            not os.path.basename(f).startswith("_"))
-
-
-def convertCtimeStringToDatetime(df):
-    # 'time' column is in (text) ctime format
-    # datetime.strptime(jsonobj['time'], "%a %b %d %H:%M:%S %Y\n")
-    # or
-    # http://stackoverflow.com/questions/26763344/convert-pandas-column-to-datetime
-    # normalize() resets the time to midnight (so it can be == vs. dates)
-    df['time'] = df['time'].apply(
-        lambda x: pandas.to_datetime(x,
-                                     infer_datetime_format=True).normalize())
-    return df
-
-
-def DOBFStoBFS(df):
-    df.loc[df.algorithm == 'DOBFS', 'algorithm'] = 'BFS'
-    return df
-
-
-def equateRGG(df):
-    df.loc[df.dataset == 'rgg_n_2_24_s0', 'dataset'] = 'rgg_n24_0.000548'
-    return df
-
-
-def normalizePRMTEPS(df):
-    df.loc[df.algorithm == 'PageRank', 'm_teps'] = df[
-        'm_teps'] * df['search_depth']
-    return df
-
-
-def gunrockVersionGPU(df):
-    df['gunrock_version_gpu'] = df[
-        'gunrock_version'] + " / " + df['gpuinfo.name']
-    return df
-
-
-def addJSONDetailsLink(df):
-    df['details'] = pandas.Series(json_input_files).values
-    df['details'] = df['details'].apply(lambda s: re.sub(
-        r'.*gunrock-output',
-        '<a href="https://github.com/gunrock/io/tree/master/gunrock-output',
-        s) + '">JSON output</a>')
-    return df
-
-
-def selectAnyOfTheseDates(dates):
-    return lambda df: df[df['time'].isin(dates)]
-
-
-def deleteZeroMTEPS(df):
-    return df[df['m_teps'] != 0]
-
-
-# user settings for this script
-root = '../gunrock-output/'
+# begin user settings for this script
+roots = ['../gunrock-output']
 fnFilterInputFiles = [fileEndsWithJSON,
                       ]
 fnPreprocessDF = [convertCtimeStringToDatetime,
@@ -89,14 +29,18 @@ fnFilterDFRows = [selectAnyOfTheseDates([datetime.date(2016, 11, 17),
                   # 2016/11/17 is gunrock-output/topc/
                   deleteZeroMTEPS,
                   ]
+fnPostprocessDF = []
 # end user settings for this script
 
 # actual program logic
 # do not modify
 
 # choose input files
-json_input_files = [os.path.join(subdir, f) for (subdir, dirs, files)
-                    in os.walk(root) for f in files]
+json_input_files = []
+for root in roots:
+    json_input_files = json_input_files + ([os.path.join(subdir, f)
+                                            for (subdir, dirs, files)
+                                            in os.walk(root) for f in files])
 # filter input files
 for fn in fnFilterInputFiles:
     json_input_files = filter(fn, json_input_files)
@@ -107,10 +51,13 @@ data_unfiltered = [json.load(open(jf)) for jf in json_input_files]
 # instead, json_normalize flattens nested dicts
 df = json_normalize(data_unfiltered)
 # http://stackoverflow.com/questions/26666919/python-pandas-add-column-in-dataframe-from-list
+df['details'] = pandas.Series(json_input_files).values
 
 for fn in fnPreprocessDF:       # alter entries / compute new entries
     df = fn(df)
 for fn in fnFilterDFRows:       # remove rows
+    df = fn(df)
+for fn in fnPostprocessDF:      # alter entries / compute new entries
     df = fn(df)
 
 # end actual program logic
---
+++ scripts/filters.py
@@ -0,0 +1,61 @@
+import pandas  # http://pandas.pydata.org
+import os      # built-in
+import re      # built-in
+
+# possible filtering functions
+
+
+def fileEndsWithJSON(f):
+    return (os.path.isfile(f) and
+            (os.path.splitext(f)[1] == ".json") and
+            not os.path.basename(f).startswith("_"))
+
+
+def convertCtimeStringToDatetime(df):
+    # 'time' column is in (text) ctime format
+    # datetime.strptime(jsonobj['time'], "%a %b %d %H:%M:%S %Y\n")
+    # or
+    # http://stackoverflow.com/questions/26763344/convert-pandas-column-to-datetime
+    # normalize() resets the time to midnight (so it can be == vs. dates)
+    df['time'] = df['time'].apply(
+        lambda x: pandas.to_datetime(x,
+                                     infer_datetime_format=True).normalize())
+    return df
+
+
+def DOBFStoBFS(df):
+    df.loc[df.algorithm == 'DOBFS', 'algorithm'] = 'BFS'
+    return df
+
+
+def equateRGG(df):
+    df.loc[df.dataset == 'rgg_n_2_24_s0', 'dataset'] = 'rgg_n24_0.000548'
+    return df
+
+
+def normalizePRMTEPS(df):
+    df.loc[df.algorithm == 'PageRank', 'm_teps'] = df[
+        'm_teps'] * df['search_depth']
+    return df
+
+
+def gunrockVersionGPU(df):
+    df['gunrock_version_gpu'] = df[
+        'gunrock_version'] + " / " + df['gpuinfo.name']
+    return df
+
+
+def addJSONDetailsLink(df):
+    df['details'] = df['details'].apply(lambda s: re.sub(
+        r'.*gunrock-output',
+        '<a href="https://github.com/gunrock/io/tree/master/gunrock-output',
+        s) + '">JSON output</a>')
+    return df
+
+
+def selectAnyOfTheseDates(dates):
+    return lambda df: df[df['time'].isin(dates)]
+
+
+def deleteZeroMTEPS(df):
+    return df[df['m_teps'] != 0]
