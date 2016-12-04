480d4976fbfcd0fbb9ba927e618143a74bf45fb4
John Owens <jowens@ece.ucdavis.edu>
2016-12-04 11:41:01 -0800

    updated to use new script structure, updated to get newest TOPC numbers, added deselectTag

--- scripts/altair_engines.py
+++ scripts/altair_engines.py
@@ -2,16 +2,12 @@
 
 from altair import *
 import pandas  # http://pandas.pydata.org
-from pandas.io.json import json_normalize
 import numpy
-import json    # built-in
-import os      # built-in
-import copy    # built-in
 import datetime
-from subprocess import Popen, PIPE, STDOUT, check_output, CalledProcessError
 
-from fileops import savefile
+from fileops import save
 from filters import *
+from logic import *
 
 name = 'engines_topc'
 
@@ -34,10 +30,14 @@ fnFilterDFRows = [
     selectAnyOfTheseDates([datetime.date(2016, 11, 17),
                            datetime.date(2016, 11, 18),
                            datetime.date(2016, 11, 20),
+                           datetime.date(2016, 11, 28),
                            datetime.date(2016, 11, 29)]),
     # 2016/11/17 is gunrock-output/topc/
-    # 2016/11/20 is {CuSha,Galois}-output/topc/
+    # 2016/11/20 is Galois-output/topc/
     # 2016/11/26 is Ligra-output/topc/
+    # 2016/11/28 is CuSha-output/topc/
+    deselectTag('topc_arch'),
+    deselectTag('do_sweep'),
     computeOtherMTEPSFromGunrock,
     deleteZeroMTEPS,
 ]
@@ -50,22 +50,8 @@ fnPostprocessDF = [
 # do not modify
 
 # choose input files
-json_input_files = []
-for root in roots:
-    json_input_files = json_input_files + ([os.path.join(subdir, f)
-                                            for (subdir, dirs, files)
-                                            in os.walk(root) for f in files])
-# filter input files
-for fn in fnFilterInputFiles:
-    json_input_files = filter(fn, json_input_files)
-
-# dump input files into dataframe
-data_unfiltered = [json.load(open(jf)) for jf in json_input_files]
-# next call used to be df = pandas.DataFrame(data_unfiltered)
-# instead, json_normalize flattens nested dicts
-df = json_normalize(data_unfiltered)
-# http://stackoverflow.com/questions/26666919/python-pandas-add-column-in-dataframe-from-list
-df['details'] = pandas.Series(json_input_files).values
+df = filesToDF(roots=roots,
+               fnFilterInputFiles=fnFilterInputFiles)
 
 for fn in fnPreprocessDF:       # alter entries / compute new entries
     df = fn(df)
@@ -110,29 +96,28 @@ for (data, caption) in [('m_teps', 'MTEPS'), ('elapsed', 'Elapsed time (ms)')]:
                     ),
     )
     print chart.to_dict(data=False)
-    plotname = '%s_%s' % (name, data)
-    for fileformat in ['html', 'png', 'svg', 'pdf']:
-        savefile(chart, name=plotname, fileformat=fileformat)
+    save(chart=chart,
+         df=df,
+         plotname='%s_%s' % (name, data),
+         formats=['html', 'svg', 'png', 'pdf'],
+         )
 
-tablefile = name + "_table.html"
-outfile = open(tablefile, 'w')
-# http://stackoverflow.com/questions/26277757/pandas-to-html-truncates-string-contents
-pandas.set_option('display.max_colwidth', -1)
-df.sort_values(['algorithm',
-                'sub_algorithm',
-                'dataset',
-                'engine',
-                'gunrock_version']).to_html(buf=outfile,
-                                            columns=['algorithm',
-                                                     'sub_algorithm',
-                                                     'dataset',
-                                                     'engine',
-                                                     'm_teps',
-                                                     'edges_visited',
-                                                     'elapsed',
-                                                     'gunrock_version',
-                                                     'gpuinfo.name',
-                                                     'details'],
-                                            index=False,
-                                            escape=False)
-outfile.close()
+save(df=df,
+     plotname=name,
+     formats=['tablehtml'],
+     sortby=['algorithm',
+             'sub_algorithm',
+             'dataset',
+             'engine',
+             'gunrock_version'],
+     columns=['algorithm',
+              'sub_algorithm',
+              'dataset',
+              'engine',
+              'm_teps',
+              'edges_visited',
+              'elapsed',
+              'gunrock_version',
+              'gpuinfo.name',
+              'details'],
+     )
