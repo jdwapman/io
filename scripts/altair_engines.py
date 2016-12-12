ef9d0df82b42ecc2fd4a2aae4b25a9aed5c94b9f
John Owens <jowens@ece.ucdavis.edu>
2016-12-12 13:51:47 -0800

    hardwired added, also replaced Gunrock+PR with different run, took some hacking. Also don't pass Infinity as a number anywhere, bad things happen.

--- scripts/altair_engines.py
+++ scripts/altair_engines.py
@@ -12,8 +12,12 @@ from logic import *
 name = 'engines_topc'
 
 # begin user settings for this script
-roots = ['../gunrock-output', '../CuSha-output',
-         '../Galois-output', '../Ligra-output', '../MapGraph-output']
+roots = ['../CuSha-output', '../Galois-output', '../Ligra-output',
+         '../MapGraph-output',
+         '../HardwiredBC-output', '../HardwiredBFS-output',
+         '../HardwiredCC-output',
+         '../gunrock-output',
+         ]
 fnFilterInputFiles = [
     fileEndsWithJSON,
 ]
@@ -25,21 +29,38 @@ fnPreprocessDF = [
     normalizePRMTEPS,
     addJSONDetailsLink,
     gunrockVersionGPU,
+    replaceWith(src='Hardwired-BC', dest='Hardwired', column='engine'),
+    replaceWith(src='Hardwired-CC', dest='Hardwired', column='engine'),
+    replaceWith(src='Hardwired-BFS', dest='Hardwired', column='engine'),
 ]
 fnFilterDFRows = [
     selectAnyOfTheseDates([datetime.date(2016, 11, 17),
                            datetime.date(2016, 11, 18),
                            datetime.date(2016, 11, 20),
+                           datetime.date(2016, 11, 25),
+                           datetime.date(2016, 11, 26),
                            datetime.date(2016, 11, 28),
-                           datetime.date(2016, 11, 29)]),
+                           datetime.date(2016, 11, 29),
+                           datetime.date(2016, 11, 30),
+                           ]),
     # 2016/11/17 is gunrock-output/topc/
     # 2016/11/20 is Galois-output/topc/
+    # 2016/11/{25,30} is MapGraph-output/topc/
+    # 2016/11/{25,26} is hardwired/topc/
     # 2016/11/26 is Ligra-output/topc/
     # 2016/11/28 is CuSha-output/topc/
     deselectTag('topc_arch'),
     deselectTag('do_sweep'),
+    deselectTag('do_sweep2'),
     computeOtherMTEPSFromGunrock,
     deleteZeroMTEPS,
+    deleteZeroElapsed,
+    # pagerank has more m_teps than pagerankdelta.
+    filterOut(value='pagerankdelta', column='sub_algorithm'),
+    # Yeah, bitvector and radii can be removed.
+    filterOut(value='bfs-bitvector', column='sub_algorithm'),
+    filterOut(value='radii', column='sub_algorithm'),
+    keepFastest(['algorithm', 'dataset', 'engine']),
 ]
 
 fnPostprocessDF = [
@@ -62,7 +83,49 @@ for fn in fnPostprocessDF:      # alter entries / compute new entries
 
 # end actual program logic
 
+# now patch this up.
+# replace all Gunrock/PR results with
+# `gunrock/io/gunrock-output/topc/optimization-switch`, tag `optimization_test_LB`.
+
+dfpatch = filesToDF(roots=['../gunrock-output/topc/optimization-switch'],
+                    fnFilterInputFiles=fnFilterInputFiles)
+for fn in [convertCtimeStringToDate,
+           selectTag('optimization_test_LB'),
+           selectAnyOfThese(column='algorithm', these=['PageRank']),
+           normalizePRMTEPS,
+           addJSONDetailsLink,
+           gunrockVersionGPU,
+           deleteZeroMTEPS,
+           ]:
+    dfpatch = fn(dfpatch)
+
+df = df.append(dfpatch)
+# next line should filter the duplicate PRs
+df = (keepFastest(['algorithm', 'dataset', 'engine']))(df)
+
+
 # now make the graph
+
+save(df=df,
+     plotname=name,
+     formats=['tablehtml'],
+     sortby=['algorithm',
+             'dataset',
+             'sub_algorithm',
+             'engine',
+             'gunrock_version'],
+     columns=['algorithm',
+              'dataset',
+              'sub_algorithm',
+              'engine',
+              'm_teps',
+              'elapsed',
+              'edges_visited',
+              'gunrock_version',
+              # 'gpuinfo.name',
+              'time',
+              'details'],
+     )
 
 for (data, caption) in [('m_teps', 'MTEPS'), ('elapsed', 'Elapsed time (ms)')]:
 
@@ -84,12 +147,17 @@ for (data, caption) in [('m_teps', 'MTEPS'), ('elapsed', 'Elapsed time (ms)')]:
             ),
             scale=Scale(type='log'),
             ),
-        color=Color('sub_algorithm:N',
-                    legend=Legend(
-                        title='Algorithmic variant',
-                    ),
-                    ),
+        # color=Color('sub_algorithm:N',
+        #             legend=Legend(
+        #                 title='Algorithmic variant',
+        #             ),
+        #             ),
         shape=Shape('engine',
+                    legend=Legend(
+                        title='Engine',
+                    ),
+                    ),
+        color=Color('engine',
                     legend=Legend(
                         title='Engine',
                     ),
@@ -99,25 +167,5 @@ for (data, caption) in [('m_teps', 'MTEPS'), ('elapsed', 'Elapsed time (ms)')]:
     save(chart=chart,
          df=df,
          plotname='%s_%s' % (name, data),
-         formats=['html', 'svg', 'png', 'pdf'],
+         formats=['json', 'html', 'svg', 'png', 'pdf'],
          )
-
-save(df=df,
-     plotname=name,
-     formats=['tablehtml'],
-     sortby=['algorithm',
-             'sub_algorithm',
-             'dataset',
-             'engine',
-             'gunrock_version'],
-     columns=['algorithm',
-              'sub_algorithm',
-              'dataset',
-              'engine',
-              'm_teps',
-              'edges_visited',
-              'elapsed',
-              'gunrock_version',
-              'gpuinfo.name',
-              'details'],
-     )
