3774c042bd6d51903fc491772e030670e5a8c9ad
John Owens <jowens@ece.ucdavis.edu>
2018-08-09 07:00:43 -0700

    added cuda 10 pr

--- scripts/altair_gunrock_v100.py
+++ scripts/altair_gunrock_v100.py
@@ -12,9 +12,9 @@ from logic import *
 name = 'gunrock_v100'
 
 # begin user settings for this script
-roots = ['../gunrock-output/5Apps.ubuntu16.04_v100x1_dev',
-         '../gunrock-output/5Apps.ubuntu16.04_v100x1_dev_sha-b8949e8',
-         '../gunrock-output/5Apps.ubuntu16.04_TitanVx1_dev_sha-b8949e8'
+roots = ['../gunrock-output/5Apps.ubuntu16.04_v100x1_dev_sha-b8949e8',
+         '../gunrock-output/5Apps.ubuntu16.04_TitanVx1_dev_sha-b8949e8',
+         '../gunrock-output/cuda10/',
          ]
 fnFilterInputFiles = [
     fileEndsWithJSON,
