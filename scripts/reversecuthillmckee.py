dc123932457840cb97e448c038e292dfffdf54ea
John Owens <jowens@ece.ucdavis.edu>
2020-06-23 10:57:31 -0700

    testing code, mostly. should be functionally the same

--- scripts/reversecuthillmckee.py
+++ scripts/reversecuthillmckee.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
+from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import reverse_cuthill_mckee
 from scipy.io import mminfo, mmread, mmwrite
 import sys, getopt
 
 
 def main(argv):
-    input_file = ""
-    output_file = ""
+    inputFile = ""
+    outputFile = ""
+    useTest = False
+    debug = False
 
     try:
-        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+        opts, args = getopt.getopt(argv, "dhi:o:t", ["ifile=", "ofile="])
     except getopt.GetoptError:
         print("{argv[0]} -i <inputfile> -o <outputfile>")
         sys.exit(2)
@@ -18,32 +21,69 @@ def main(argv):
         if opt == "-h":
             print(f"{argv[0]} -i <inputfile> -o <outputfile>")
             sys.exit()
+        elif opt == "-d":
+            debug = True
+        elif opt == "-t":
+            useTest = True
         elif opt in ("-i", "--ifile"):
-            input_file = arg
+            inputFile = arg
         elif opt in ("-o", "--ofile"):
-            output_file = arg
+            outputFile = arg
 
-    print(input_file, output_file)
+    if useTest:
+        # https://www.geeksforgeeks.org/reverse-cuthill-mckee-algorithm/
+        csrMatrix = csr_matrix(
+            [
+                [0, 1, 0, 0, 0, 0, 1, 0, 1, 0],
+                [1, 0, 0, 0, 1, 0, 1, 0, 0, 1],
+                [0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 0, 0, 1, 0],
+                [0, 1, 1, 1, 0, 1, 0, 0, 0, 1],
+                [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+                [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+                [1, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+                [0, 1, 0, 0, 1, 0, 0, 1, 0, 0],
+            ]
+        )
+        # I believe the answer is
+        # [[0 1 1 0 0 0 0 0 0 0]
+        #  [1 0 0 1 0 0 1 0 0 0]
+        #  [1 0 0 0 0 1 0 1 0 0]
+        #  [0 1 0 0 1 0 0 1 0 0]
+        #  [0 0 0 1 0 0 0 1 0 0]
+        #  [0 0 1 0 0 0 1 1 1 0]
+        #  [0 1 0 0 0 1 0 0 1 0]
+        #  [0 0 1 1 1 1 0 0 0 1]
+        #  [0 0 0 0 0 1 1 0 0 1]
+        #  [0 0 0 0 0 0 0 1 1 0]]
+        # I can't promise this is RCM-correct, but it's the same graph.
+        # Note it's symmetric, and mmwrite will output only the lower triangle
+    else:
+        if not inputFile:
+            inputFile = sys.stdin
 
-    # parse command line
-    if not input_file:
-        input_file = sys.stdin
-
-    coo_matrix = mmread(input_file)  # returns coo_matrix
-    csr_matrix = coo_matrix.tocsr()
+        cooMatrix = mmread(inputFile)  # returns coo_matrix
+        csrMatrix = cooMatrix.tocsr()
 
     # Returns the permutation array that orders a sparse CSR or CSC matrix in Reverse-Cuthill McKee ordering.
     # https://scicomp.stackexchange.com/questions/24817/applying-the-result-of-cuthill-mckee-in-scipy   couldn't make that work
     # instead used https://github.com/brightway-lca/brightway2-analyzer/blob/master/bw2analyzer/matrix_grapher.py
-    perm = reverse_cuthill_mckee(csr_matrix)
-    rcm_matrix = csr_matrix[perm, :][:, perm]
+    perm = reverse_cuthill_mckee(csrMatrix)
+    if debug:
+        print(f"Here's my input matrix, called csrMatrix:\n{csrMatrix.toarray()}")
+        print(f"Then I call perm = reverse_cuthill_mckee(csrMatrix)\nperm = {perm}")
+        print(f"csrMatrix[perm, :][:, perm]:\n{csrMatrix[perm, :][:, perm].toarray()}")
+        print(f"csrMatrix[perm][perm]:\n{csrMatrix[perm][perm].toarray()}")
+        print(f"csrMatrix[perm, perm]:\n{csrMatrix[perm, perm]}")
+    rcmMatrix = csrMatrix[perm, :][:, perm]
 
     # now save it
 
-    if output_file:
-        mmwrite(output_file, rcm_matrix)
+    if outputFile:
+        mmwrite(outputFile, rcmMatrix)  # , symmetry="general")
     else:
-        sys.stdout.write(rcm_matrix)
+        sys.stdout.write(rcmMatrix)
 
 
 if __name__ == "__main__":
