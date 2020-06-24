e5fadbfcc6e1e5470dc3b650277b21770542a01a
John Owens <jowens@ece.ucdavis.edu>
2020-06-23 21:30:47 -0700

    added a filesystem try-catch

--- scripts/logic.py
+++ scripts/logic.py
@@ -1,6 +1,6 @@
-import os      # built-in
-import sys     # built-in
-import json    # built-in
+import os  # built-in
+import sys  # built-in
+import json  # built-in
 from json.decoder import JSONDecodeError
 import pandas  # http://pandas.pydata.org
 
@@ -8,9 +8,13 @@ import pandas  # http://pandas.pydata.org
 def filesToDF(roots, fnFilterInputFiles):
     json_input_files = []
     for root in roots:
-        json_input_files = json_input_files + ([os.path.join(subdir, f)
-                                                for (subdir, dirs, files)
-                                                in os.walk(root) for f in files])
+        json_input_files = json_input_files + (
+            [
+                os.path.join(subdir, f)
+                for (subdir, dirs, files) in os.walk(root)
+                for f in files
+            ]
+        )
     # filter input files
     for fn in fnFilterInputFiles:
         json_input_files = list(filter(fn, json_input_files))
@@ -24,19 +28,21 @@ def filesToDF(roots, fnFilterInputFiles):
             jf = json.load(open(jf_file))
         except JSONDecodeError:
             print("Invalid JSON file: %s" % jf_file, file=sys.stderr)
+        except OSError:
+            print("Input/output error: %s" % jf_file, file=sys.stderr)
         if isinstance(jf, list):  # assume list of dicts
             for d in jf:
-                d['details'] = jf_file
+                d["details"] = jf_file
                 data_unfiltered.append(d)
         else:  # not a list, assume dict
-            jf['details'] = jf_file
+            jf["details"] = jf_file
             data_unfiltered.append(jf)
 
     # dump input files into dataframe
     # data_unfiltered = [json.load(open(jf)) for jf in json_input_files]
     # next call used to be df = pandas.DataFrame(data_unfiltered)
     # instead, json_normalize flattens nested dicts
-    df = pandas.json_normalize(data_unfiltered, sep='_')
+    df = pandas.json_normalize(data_unfiltered, sep="_")
     # http://stackoverflow.com/questions/26666919/python-pandas-add-column-in-dataframe-from-list
     # df['details'] = pandas.Series(json_input_files).values
     return df
